2d4ad3165bd4269ce1f47111a9134bc3ef87c1d5
kylelbs <kyle.lebrissaget@gmail.com>
2022-03-10 22:29:37 +0100

    implemented computerPlay, playRound and game functions

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 console.log("Hello world !")
+
+function computerPlay(){
+    values = ["Rock", "Paper", "Scissors"]
+    random_number = Math.floor(Math.random() * 3)
+    return values[random_number]
+}
+
+function playRound(user1, user2){
+    if (user1 == "Rock" && user2 == "Rock" || user1 == "Paper" && user2 == "Paper" || user1 == "Scissors" && user2 == "Scissors"){
+        return "tie"
+    }
+    else if (user1 == "Rock" && user2 == "Scissors" || user1 == "Paper" && user2 == "Rock" || user1 == "Scissors" && user2 == "Paper"){
+        return "win"
+    }
+    else {
+        return "loose"
+    }
+}
+
+function game(){
+    for (let i= 0; i<5; i++) {
+        result = playRound(userSelection, computerSelection)
+        if 
+    }
+}
+
+userSelection = prompt("Rock, Paper or Scissors ?: ")
+computerSelection = computerPlay()
+playRound(userSelection, computerSelection)
