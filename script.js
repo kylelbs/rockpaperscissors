9039e2de0d9e76cb1efccd0f15dd1ad5ff1b833d
kylelbs <kyle.lebrissaget@gmail.com>
2022-08-01 21:19:28 +0200

    implemented the game with DOM manipulation

--- script.js
+++ script.js
@@ -1,49 +1,73 @@
-// console.log("Hello world !");
+const buttons = document.querySelectorAll("button");
+const buttonsdiv = document.querySelector(".buttons");
 
-// function computerPlay() {
-//     values = ["Rock", "Paper", "Scissors"];
-//     random_number = Math.floor(Math.random() * 3);
-//     return values[random_number];
-// };
+let textScore = document.createElement("p");
+textScore.textContent = "";
+textScore.classList.add("textScore");
+buttonsdiv.appendChild(textScore);
 
-// function playRound(user1, user2) {
-//     if (user1 == "Rock" && user2 == "Rock" || user1 == "Paper" && user2 == "Paper" || user1 == "Scissors" && user2 == "Scissors") {
-//         return 2;
-//     }
-//     else if (user1 == "Rock" && user2 == "Scissors" || user1 == "Paper" && user2 == "Rock" || user1 == "Scissors" && user2 == "Paper") {
-//         return 1;
-//     }
-//     else {
-//         return 0;
-//     };
-// };
+let numberScore = document.createElement("p");
+numberScore.textContent = "";
+textScore.classList.add("numberScore");
+buttonsdiv.appendChild(numberScore);
 
-// function game() {
-//     let computerScore = 0;
-//     let userScore = 0;
-//     for (let i = 0; i < 5; i++) {
-//         userSelection = prompt("Rock, Paper or Scissors ?: ");
-//         computerSelection = computerPlay();
-//         result = playRound(userSelection, computerSelection);
-//         if (result == 1) {
-//             userScore++;
-//             console.log("You won ! +1 point");
-//         }
-//         else if (result == 0) {
-//             computerScore++;
-//             console.log("You loose ! +1 point for computer");
-//         }
-//         else {
-//             console.log("Tie !");
-//         };
-//     };
-//     if (computerScore > userScore) {
-//         console.log(`You loose, sorry ! The result are : ${computerScore} for computer and ${userScore} for you.`);
-//     }
-//     else {
-//         console.log(`You won, congratulations ! The result are : ${computerScore} for computer and ${userScore} for you.`);
-//     }
 
-// };
+function computerPlay() {
+    values = ["rock", "paper", "scissors"];
+    random_number = Math.floor(Math.random() * 3);
+    return values[random_number];
+};
 
-// game();
+function playRound(user1, user2) {
+    if (user1 == "rock" && user2 == "rock" || user1 == "paper" && user2 == "paper" || user1 == "scissors" && user2 == "scissors") {
+        return 2;
+    }
+    else if (user1 == "rock" && user2 == "scissors" || user1 == "paper" && user2 == "rock" || user1 == "scissors" && user2 == "paper") {
+        return 1;
+    }
+    else {
+        return 0;
+    };
+};
+
+let computerScore = 0;
+let userScore = 0;
+let userSelection;
+let round = 5;
+
+buttons.forEach(addEventListener('click', function (e) {
+    if (round != 0) {
+        userSelection = e.target.id;
+        computerSelection = computerPlay();
+        result = playRound(userSelection, computerSelection);
+        if (result == 1) {
+            userScore++;
+            textScore.textContent = "You won ! +1 point";
+        }
+        else if (result == 0) {
+            computerScore++;
+            textScore.textContent = "You loose ! +1 point for computer";
+        }
+        else {
+            textScore.textContent = "Tie !";
+        };
+        round--;
+        numberScore.textContent = `Score - You : ${userScore} Computer: ${computerScore}`;
+        if (round == 0) {
+            if (computerScore > userScore) {
+                textScore.textContent = `You loose, sorry ! The results are : ${computerScore} for computer and ${userScore} for you.`;
+            }
+            else if (computerScore == userScore) {
+                textScore.textContent = `That's a tie ! The results are : ${computerScore} for computer and ${userScore} for you.`;
+            }
+            else {
+                textScore.textContent = `You won, congratulations ! The results are : ${computerScore} for computer and ${userScore} for you.`;
+            }
+            round = 5;
+            userScore = 0;
+            computerScore = 0;
+            numberScore.textContent = "";
+        };
+        // console.log(round);
+    };
+}));
